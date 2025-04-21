1b21232661e3df8eb9f31f5aef1a75e0631a67ef
nyitraibence <nyitrai100@gmail.com>
2025-04-21 09:05:49 +0100

    pdf reader works

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter folder path: ");
+        string folderPath = Console.ReadLine();
+
+        Console.Write("Enter keyword to search: ");
+        string keyword = Console.ReadLine();
+
+        var pdfFiles = Directory.GetFiles(folderPath, "*.pdf");
+
+        foreach (var file in pdfFiles)
+        {
+            using (PdfDocument document = PdfDocument.Open(file))
+            {
+                string text = "";
+                foreach (Page page in document.GetPages())
+                {
+                    text += page.Text;
+                }
+
+                if (text.IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    Console.WriteLine(Path.GetFileName(file));
+                }
+            }
+        }
+
+        Console.WriteLine("Search complete.");
+    }
+}
